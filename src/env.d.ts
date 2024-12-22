743a5d0060ced65dfa071bacfabffcd4033625c1
lexdeit <evillavicencio@delvelk.mx>
2024-12-22 00:18:42 -0600

    init: first commit

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference path="../.astro/types.d.ts" />
+ 
+declare namespace App {
+    // Note: 'import {} from ""' syntax does not work in .d.ts files.
+    interface Locals {
+        user: import("better-auth").User | null;
+        session: import("better-auth").Session | null;
+    }
+}
---
+++ src/pages/api/auth/[...all].js
@@ -0,0 +1,9 @@
+import { auth } from "../../../../lib/auth"; 
+import type { APIRoute } from "astro";
+
+
+export const prerender = false;
+ 
+export const ALL: APIRoute = async (ctx) => {
+	return auth.handler(ctx.request);
+};
